c5252349394a137deb6b28bb3672fc15cf091fa8
Iskander Haziev <gvalmon@gmail.com>
2016-02-03 16:43:58 +0300

    show info if package not found

--- lib/rdm/source_installer.rb
+++ lib/rdm/source_installer.rb
@@ -5,18 +5,20 @@ class Rdm::SourceInstaller
       source_content = File.open(source_path).read
       source_parser.parse(source_content).each do |package_path|
         full_path = File.join(File.dirname(source_path), package_path, Rdm::PACKAGE_FILENAME)
-        lock(full_path, source_path)
+        lock(full_path, source_path: source_path, package_path: package_path)
       end
     end
 
     # Lock package on installation
-    def lock(package_path, source_path)
-      package_content = File.open(package_path).read
+    def lock(full_package_path, source_path:, package_path:)
+      package_content = File.open(full_package_path).read
 
       locked = "source \"#{source_path}\"\r\n\r\n"
       locked += package_content
 
-      File.open("#{package_path}.lock", "w+").write(locked)
+      File.open("#{full_package_path}.lock", "w+").write(locked)
+    rescue Errno::ENOENT
+      puts "Can't find package: #{package_path}"
     end
 
     private
