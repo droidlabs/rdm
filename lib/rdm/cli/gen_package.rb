025eb82e313f30e4c8a7e3ad126f81b39e6c3836
Roman Heinrich <roman.heinrich@gmail.com>
2016-12-15 21:24:26 +0100

    use Rdm::PackageGenerator in CLI::GenPackage class (tmp)

--- lib/rdm/cli/gen_package.rb
+++ lib/rdm/cli/gen_package.rb
@@ -17,6 +17,32 @@ module Rdm
 
       def run
         puts "running... with #{self.inspect}"
+        check_preconditions!
+        begin
+          generate
+        rescue Errno::ENOENT => e
+          puts "Error occurred. Possible reasons:\n #{Rdm::SOURCE_FILENAME} not found. Please run on directory containing #{Rdm::SOURCE_FILENAME} \n#{e.inspect}"
+        rescue Rdm::Errors::PackageExists
+          puts "Error. Package already exist. Package was not generated"
+        rescue Rdm::Errors::PackageDirExists
+          puts "Error. Directory #{package_relative_path} exists. Package was not generated"
+        end
+      end
+
+      def generate
+        Rdm::PackageGenerator.generate_package(
+          self.current_dir,
+          self.package_name,
+          self.package_relative_path,
+          self.skip_tests
+        )
+      end
+
+      def check_preconditions!
+        if package_name.empty?
+          puts 'Package name was not specified!'
+          exit 1
+        end
       end
     end
   end
