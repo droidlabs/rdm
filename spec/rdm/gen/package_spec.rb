012850969a8f5cbcf01b9dabe8bd10e38e2bb658
Roman Heinrich <roman.heinrich@gmail.com>
2016-12-29 22:19:08 +0100

    specs for Rdm::Gen::Package
    problem: 
    - we have no specs for the generators, that kinda sucks...
    
    
    solution: 
    - well... write a really simple spec for the happy path!

---
+++ spec/rdm/gen/package_spec.rb
@@ -0,0 +1,52 @@
+require "spec_helper"
+
+describe Rdm::Gen::Package do
+  include SetupHelper
+
+  def generate_project!
+    Rdm::Gen::Package.generate_package(
+      current_dir: project_dir,
+      package_name: "some",
+      package_relative_path: "domain/some",
+      skip_rspec: true
+    )
+  end
+
+  def ensure_exists(file)
+    expect(File.exists?(file)).to be true
+  end
+
+  def ensure_content(file, content)
+    expect(File.read(file)).to match(content)
+  end
+
+  context "sample package" do
+    before :all do
+      fresh_project
+      generate_project!
+    end
+
+    after :all do
+      clean_tmp
+    end
+
+    it "has generated correct files" do
+      FileUtils.cd(project_dir) do
+        ensure_exists("domain/some/Package.rb")
+        ensure_exists("domain/some/package/some.rb")
+        ensure_exists("domain/some/package/some/")
+        ensure_exists("domain/some/bin/console")
+        ensure_exists("domain/some/spec/spec_helper.rb")
+        ensure_exists("domain/some/.rspec")
+        ensure_exists("domain/some/.gitignore")
+        ensure_content("domain/some/package/some.rb", "module Some\n\nend\n")
+      end
+    end
+
+    it "has added new entry to Rdm.packages" do
+      FileUtils.cd(project_dir) do
+        ensure_content("Rdm.packages", "package 'domain/some'")
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,27 @@ RSpec.configure do |config|
   config.color = true
 end
 
+
+module SetupHelper
+  def clean_tmp
+    FileUtils.rm_rf(tmp_dir)
+  end
+
+  def fresh_project
+    clean_tmp
+    FileUtils.mkdir_p(tmp_dir)
+    FileUtils.cp_r(example_src, tmp_dir)
+  end
+
+  def project_dir
+    File.join(tmp_dir, "example")
+  end
+
+  def tmp_dir
+    File.join(File.dirname(__FILE__), "tmp/projects")
+  end
+
+  def example_src
+    File.join(File.dirname(__FILE__), "../example/")
+  end
+end
