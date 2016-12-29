cbc3604465a10624b1c3695a76573b354746b6c6
Roman Heinrich <roman.heinrich@gmail.com>
2016-12-29 17:05:57 +0100

    use source_locator in auto_updater

--- lib/rdm/auto_updater.rb
+++ lib/rdm/auto_updater.rb
@@ -13,29 +13,14 @@ module Rdm
 
     def update
       begin
-        source_path = find_source_path_in_hierarchy(path)
         Rdm::SourceInstaller.install(source_path)
       rescue Rdm::Errors::SourceFileDoesNotExist => e
         puts "*** #{path} does not include any #{Rdm::SOURCE_FILENAME} in its tree hierarchy!"
       end
     end
 
-    def find_source_path_in_hierarchy(some_path)
-      some_path = File.expand_path(some_path)
-      raise SourceFileDoesNotExist if some_path == "/"
-      if is_present?(some_path)
-        return potential_file(some_path)
-      else
-        find_source_path_in_hierarchy(File.dirname(some_path))
-      end
-    end
-
-    def is_present?(some_path)
-      File.exists?(potential_file(some_path))
-    end
-
-    def potential_file(some_path)
-      File.join(some_path, Rdm::SOURCE_FILENAME)
+    def source_path
+      Rdm::SourceLocator.locate(path)
     end
   end
 end
